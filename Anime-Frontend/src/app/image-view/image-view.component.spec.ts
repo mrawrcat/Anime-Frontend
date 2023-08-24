c26af1fce3699baa72f78c810124435d5fd02697
mrawrcat <danwen@penguin>
2023-08-23 22:30:13 -0400

    started on tests

--- Anime-Frontend/src/app/image-view/image-view.component.spec.ts
+++ Anime-Frontend/src/app/image-view/image-view.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImageViewComponent } from './image-view.component';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ImageViewComponent', () => {
   let component: ImageViewComponent;
@@ -8,6 +13,13 @@ describe('ImageViewComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MaterialModule,
+        FormsModule
+      ],
       declarations: [ImageViewComponent]
     });
     fixture = TestBed.createComponent(ImageViewComponent);
